61c5fa7c8b530317e5732d09e50af74c43f91101
hojatqazi-dev <hojat.qazi@googlemail.com>
2026-03-16 16:45:30 +0430

    Initialize hotel reservation project with Spring Boot

---
+++ src/test/java/Hotel_Booking_System/com/hojat/HotelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Hotel_Booking_System.com.hojat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
